6e200cefba26d7341b8bb3bef681c245ae3e2e58
Dana <dana_ciobo@yahoo.com>
2024-11-29 19:43:21 +0000

    initial refactored

--- milestone_2.py
+++ milestone_2.py
@@ -3,16 +3,19 @@ import random
 
 word_list = ["banana", "raspberry", "blueberry", "watermelon", "mangosteen"]
 
-print(word_list)
+def get_random_word(list):
+  print(random.choice(list))
+  return random.choice(list)
 # %%
+def is_letter():
+  while True:
+    guess = input("Please enter a single letter: ")
 
-word = random.choice(word_list)
+    if len(guess) == 1 and guess.isalpha():
+      print("Good guess!")
+      return guess
+    else:
+     print("Oops! That is not a valid input." )
 
-print(word)
-# %%
-guess = input("Please enter a single letter: ")
-
-if len(guess) == 1 and guess.isalpha():
-  print("Good guess!")
-else:
-  print("Oops! That is not a valid input." )
+get_random_word(word_list)
+is_letter()
